161b71356f91f982b8217615d3b3c1df6fd618fb
DavidWaichari <dwaichari@gmail.com>
2024-03-04 11:13:58 +0300

    add models for User and Profile and update their migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,29 +17,10 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['first_name', 'middle_name', 'last_name'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
