be9426a769b4403203affd2a3f8e8f40ff91df53
DavidWaichari <dwaichari@gmail.com>
2024-03-04 12:31:14 +0300

    format email template

--- resources/views/emails/job_application.blade.php
+++ resources/views/emails/job_application.blade.php
@@ -1,13 +1,14 @@
 <!-- job_application.blade.php -->
 
-Dear Hiring Manager,
+Dear Hiring Manager, <br>
 
-My name is {{ $name }}. I am {{ $age }} old. I am applying for a full-stack Laravel developer position at your organization.
-I believe I am the most qualified candidate with {{ $experience }} years of experience and the zeal to create competitive, easy to use, and well-structured software.
+My name is {{ $name }}. I am {{ $age }} old. I am applying for a full-stack Laravel developer position at your organization. <br>
 
-I hope to hear from you soon through my contact {{ $phone }}.
+I believe I am the most qualified candidate with {{ $experience }} years of experience and the zeal to create competitive, easy to use, and well-structured software. <br>
 
-Thank you in advance.
+I hope to hear from you soon through my contact {{ $phone }}. <br>
 
-Best regards,
+Thank you in advance. <br>
+
+Best regards, <br>
 {{ $name }}
