339156137c8b73292fa791b2bbb328adad26cd23
DavidWaichari <dwaichari@gmail.com>
2024-03-04 11:16:03 +0300

    update profiles models

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['date_of_birth', 'passport_photo', 'years_of_experience', 'phone_number', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
